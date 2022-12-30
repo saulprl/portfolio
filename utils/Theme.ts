5aaee68fa3f050dd869f95aedeb3c0cd9fe55010
Saul <phasecross@hotmail.com>
2022-12-30 12:05:20 -0700

    Add utils folder

---
+++ utils/Theme.ts
@@ -0,0 +1,53 @@
+import { createTheme } from "@mui/material";
+
+export const lightTheme = createTheme(
+  {
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#EE588A",
+      },
+      secondary: {
+        main: "#7DA1A0",
+      },
+      success: {
+        main: "#93DD2C",
+      },
+      warning: {
+        main: "#FFD685",
+      },
+      error: {
+        main: "#FE8534",
+      },
+      background: {
+        default: "#DEE4E7",
+      },
+    },
+  },
+);
+
+export const darkTheme = createTheme(
+  {
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#BA124A",
+      },
+      secondary: {
+        main: "#455F5E",
+      },
+      success: {
+        main: "#507B14",
+      },
+      warning: {
+        main: "#FFAD0A",
+      },
+      error: {
+        main: "#CB5201",
+      },
+      background: {
+        default: "#282c34",
+      },
+    },
+  },
+);
