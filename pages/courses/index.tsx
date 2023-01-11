6bfc96629906eb74dae5b4847296b652f01afc96
Saul <phasecross@hotmail.com>
2023-01-11 12:57:52 -0700

    Add courses main page

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,7 +1,26 @@
-import Head from "next/head";
-import MainContent from "../../components/UI/MainContent";
+import { FC } from "react";
 
-const CoursesPage = () => {
+import Head from "next/head";
+
+import MainContent from "../../components/UI/MainContent";
+import CourseList from "../../components/Courses/CourseList";
+
+import { loadCourses } from "../../lib/loadCourses";
+
+import type { Course } from "../../models/Course";
+import { Box } from "@mui/material";
+
+import classes from "../../styles/Courses.module.css";
+
+interface CourseData {
+  courses: Course[];
+}
+
+interface Props {
+  data: CourseData;
+}
+
+const CoursesPage: FC<Props> = (props: Props) => {
   return (
     <>
       <Head>
@@ -14,10 +33,22 @@ const CoursesPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainContent title="Courses">
-        <p>Crs</p>
+        <Box component="section" className={classes["page-content"]}>
+          <CourseList courses={props.data.courses} />
+        </Box>
       </MainContent>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const data: CourseData = await loadCourses();
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default CoursesPage;
