94ea3d117da0d1a8406bc700f81e0c49d5b8991b
Saul <phasecross@hotmail.com>
2023-01-16 08:36:27 -0700

    Add dictionary prop and interface, add locale toggle button

--- components/UI/MainAppBar.tsx
+++ components/UI/MainAppBar.tsx
@@ -1,15 +1,32 @@
 import { FC, useState } from "react";
 
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-import { Menu } from "@mui/icons-material";
+import { useRouter } from "next/router";
+
+import { AppBar, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { Language, Menu } from "@mui/icons-material";
 
 import MainDrawer from "./MainDrawer";
 
+interface Dictionary {
+  buttons: {
+    home: string;
+    projects: string;
+    courses: string;
+    darkMode: string;
+  };
+  tooltips: {
+    lightMode: string;
+    darkMode: string;
+    locale: string;
+  };
+}
 interface Props {
+  dictionary: Dictionary;
   onToggleTheme: () => void;
 }
 
 const MainAppBar: FC<Props> = (props: Props) => {
+  const router = useRouter();
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   const openDrawerHandler = () => {
@@ -23,6 +40,7 @@ const MainAppBar: FC<Props> = (props: Props) => {
   return (
     <>
       <MainDrawer
+        dictionary={props.dictionary}
         onToggleTheme={props.onToggleTheme}
         open={drawerOpen}
         onClose={closeDrawerHandler}
@@ -42,6 +60,23 @@ const MainAppBar: FC<Props> = (props: Props) => {
             <Menu />
           </IconButton>
           <Typography variant="h6">saulprl&apos;s portfolio</Typography>
+          <Button
+            variant="text"
+            endIcon={<Language />}
+            onClick={() =>
+              router.push(router.pathname, undefined, {
+                locale: router.locale === "en-US" ? "es-MX" : "en-US",
+              })
+            }
+            sx={{
+              color: "white",
+              fontWeight: "bold",
+              position: "absolute",
+              right: "16px",
+            }}
+          >
+            {router.locale === "en-US" ? "EN" : "ES"}
+          </Button>
         </Toolbar>
       </AppBar>
     </>
