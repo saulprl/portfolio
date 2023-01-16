de9029badd8119e2d89f6c9cdd50921027e68a18
Saul <phasecross@hotmail.com>
2023-01-16 12:15:13 -0700

    Add ts-results usage for promise

--- pages/api/technologies.ts
+++ pages/api/technologies.ts
@@ -2,6 +2,7 @@
 
 import path from "path";
 import { promises as fs } from "fs";
+import { Ok, Err, Result } from "ts-results";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -13,17 +14,22 @@ interface Data {
   languages: Technology;
   frameworks: Technology;
   databases: Technology;
-};
+}
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const jsonDirectory = path.join(process.cwd(), "json");
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Result<Data, Error>>
+) => {
+  try {
+    const jsonPath = path.join(process.cwd(), "json", "technologies.jsn");
 
-  const fileContents = await fs.readFile(
-    jsonDirectory + "/technologies.json",
-    "utf8"
-  );
+    const fileContents = await fs.readFile(jsonPath, "utf8");
 
-  res.status(200).json(JSON.parse(fileContents));
+    res.status(200).json(Ok(JSON.parse(fileContents) as Data));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(Err(error as Error));
+  }
 };
 
 export default handler;
