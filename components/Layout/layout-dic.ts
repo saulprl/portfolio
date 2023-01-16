c61e1946ad3eaa2d0763ffde63f3bdc05515e2c6
Saul <phasecross@hotmail.com>
2023-01-16 08:22:58 -0700

    Add a layout dictionary for locales

---
+++ components/Layout/layout-dic.ts
@@ -0,0 +1,32 @@
+const LayoutDictionary = {
+  "en-US": {
+    buttons: {
+      home: "Home",
+      projects: "Projects",
+      courses: "Courses",
+      darkMode: "Dark mode",
+    },
+    tooltips: {
+      lightMode: "Light mode",
+      darkMode: "Dark mode",
+      locale: "Toggle locale",
+      // resume: "Resume",
+    },
+  },
+  "es-MX": {
+    buttons: {
+      home: "Inicio",
+      projects: "Proyectos",
+      courses: "Cursos",
+      darkMode: "Tema oscuro",
+    },
+    tooltips: {
+      lightMode: "Tema claro",
+      darkMode: "Tema oscuro",
+      locale: "Alternar idioma",
+      // resume: "Currículum",
+    },
+  },
+};
+
+export default LayoutDictionary;
