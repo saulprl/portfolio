10fab9039f665decdbd46158ed8d0a3b55e236ce
Saúl Ramos <phasecross@hotmail.com>
2023-12-13 02:25:22 -0700

    Update hero

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -8,16 +8,18 @@ export const Hero = () => {
     <Section>
       <div className="flex w-full flex-col justify-center gap-4 rounded-md border-2 border-muted p-4 text-center md:text-start">
         <h1 className="text-3xl font-medium">
-          Hello, my name is <strong className="text-primary">Saúl</strong> and
-          I&apos;m a{" "}
+          Hello, my name is{" "}
+          <strong className="font-mono text-primary">Saúl</strong> and I&apos;m
+          a{" "}
           <span className="bg-accent font-mono font-bold text-white">
             software developer
           </span>
         </h1>
-        <h2 className="">
+        <hr className="w-full rounded-full border-2 border-primary md:w-2/5" />
+        <p>
           I&apos;m a passionate learner and enjoy making a positive impact in
           people through software development
-        </h2>
+        </p>
         <div className="flex flex-grow" />
         <div className="flex w-full items-center justify-center md:justify-start">
           <Contact />
