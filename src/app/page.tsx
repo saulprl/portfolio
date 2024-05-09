dadbb462b7b4deaf5c230f91322b817fe4f1ba6d
saulprl <phasecross@hotmail.com>
2024-05-08 18:00:26 -0700

    Add SectionTitle component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Hero } from "@/components/hero/hero";
 import { ProjectsSection } from "@/components/projects-section/projects-section";
+import { SectionTitle } from "@/components/section-title/section-title";
 import { ToolsSection } from "@/components/tools-section/tools-section";
 
 export default function Home() {
@@ -7,19 +8,21 @@ export default function Home() {
     <>
       <main className="mx-auto flex max-w-5xl flex-col items-center gap-4 p-2">
         <Hero />
-        <h2 className="text-center font-mono text-3xl font-bold text-muted">
+        <SectionTitle>Projects</SectionTitle>
+        {/* <h2 className="text-center font-mono text-3xl font-bold text-muted">
           <span className="rounded-sm border-2 border-muted bg-secondary px-px text-white">
             Pro
           </span>
           jects
-        </h2>
+        </h2> */}
         <ProjectsSection />
-        <h2 className="text-center font-mono text-3xl font-bold text-muted">
+        <SectionTitle>Frameworks & Tools</SectionTitle>
+        {/* <h2 className="text-center font-mono text-3xl font-bold text-muted">
           <span className="rounded-sm border-2 border-muted bg-secondary px-px text-white">
             Frameworks
           </span>{" "}
           & Tools
-        </h2>
+        </h2> */}
         <ToolsSection />
       </main>
     </>
---
+++ src/components/section-title/section-title.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { FC, HTMLAttributes, ReactNode } from "react";
+
+interface Props {
+  className?: HTMLAttributes<HTMLHeadingElement>["className"];
+  children: ReactNode;
+}
+
+export const SectionTitle: FC<Props> = ({ children, className }) => {
+  return (
+    <h2
+      className={cn(
+        "rounded border-2 border-muted bg-primary px-2 py-1 font-mono text-3xl font-semibold tracking-wide text-white",
+        className,
+      )}
+    >
+      {children}
+    </h2>
+  );
+};
